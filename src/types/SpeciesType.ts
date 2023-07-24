eb46e1b3742504f7b333a3e8d5d76587bf590b1a
Satish K <satish.appanasamy@gmail.com>
2023-07-24 15:29:33 +0800

    feat: Species Type - created Type file for Species

---
+++ src/types/SpeciesType.ts
@@ -0,0 +1,19 @@
+interface SpeciesType {
+    name: string,
+    classification: string,
+    designation: string,
+    average_height: string,
+    skin_colors: string,
+    hair_colors: string,
+    eye_colors: string,
+    average_lifespan: string,
+    homeworld: URL,
+    language: string,
+    people: URL[],
+    films: URL[],
+    created: string,
+    edited: string,
+    url: URL
+}
+
+export default SpeciesType;
