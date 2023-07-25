68626419cbb2ebb588812dc741990bcee7130249
Satish K <satish.appanasamy@gmail.com>
2023-07-25 20:36:16 +0800

    refactor: added data clean up where there is mammals and mammal

--- src/DataSanitise.ts
+++ src/DataSanitise.ts
@@ -1,12 +1,23 @@
 import DemoData from "./DemoData";
+import {isNullOrUndef} from "chart.js/helpers";
+import SpeciesType from "./types/SpeciesType";
 
 const DataSanitise = DemoData.map((item) => {
+    const updatedData = {...item};
     // swap out average_height parameter in item to 0 if NaN
-
-    console.log(parseInt(item.average_height));
     if (isNaN(parseInt(item.average_height))) {
-        return {...item, average_height: '0'};
-    } return item;
+        updatedData.average_height = '0';
+    }
+
+    if(isNullOrUndef(item.homeworld)) {
+        updatedData.homeworld = '';
+    }
+
+    // cleanup classification when its mammals to mammal
+    if(item.classification === 'mammals') {
+        updatedData.classification = 'mammal';
+    }
+    return updatedData;
 })
 
-export  default  DataSanitise;
+export default DataSanitise;
