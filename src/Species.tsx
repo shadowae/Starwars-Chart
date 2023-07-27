046ade35594ad5f7bd521f367ec033178f672b59
Satish K <satish.appanasamy@gmail.com>
2023-07-27 12:18:08 +0800

    refactor: removed specified pie chart file in favour of generic pie chart

--- src/Species.tsx
+++ src/Species.tsx
@@ -4,7 +4,6 @@ import {useEffect} from "react";
 import {fetchAllSpeciesData} from "./API/getSpecies";
 import {SpeciesBarChart} from "./SpeciesBarChart";
 import './Species.css'
-import SpeciesPieChart from "./SpeciesPieChart";
 import GenericPieChart from "./GenericPieChart";
 import useSpeciesStore from "./Zustand/SpeciesState";
 const Species = () => {
@@ -28,10 +27,6 @@ const Species = () => {
             <div className={'species-page-chart'}>
                 <SpeciesBarChart dataSet={speciesData}/>
             </div>
-            <div className={'species-page-chart'}>
-                <SpeciesPieChart speciesData={speciesData} />
-            </div>
-
         </div>
     )
 }
