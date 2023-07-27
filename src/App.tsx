e288890b2f5be9b166c9aacc778157bd45112a5d
Satish K <satish.appanasamy@gmail.com>
2023-07-27 12:27:05 +0800

    feature: added routing to the app - basic page of Home is created - second route to Species is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Import 'Routes' from react-router-dom
 import "./styles.css";
 import Species from "./Species";
+import Home from "./Home"; // Replace "Home" with the component you want to show on the home page
 
 export default function App() {
-  return (
-    <div className="App">
-        <Species/>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/species">Species</Link>
+                        </li>
+                    </ul>
+                </nav>
+                
+                {/* Use 'Routes' to wrap multiple 'Route' components */}
+                <Routes>
+                    <Route path="/species" element={<Species />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
