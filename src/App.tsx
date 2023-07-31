7c93e49c221de01f80cae2b75b7bee7a06c176b1
Satish K <satish.appanasamy@gmail.com>
2023-07-31 17:22:53 +0800

    feature: add navbar to App page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link as RouterLink, LinkProps as RouterLinkProps} from "react-router-dom";
 import "./styles.css";
 import Home from "./Home";
 import Species from "./Species";
 import People from './People'
-
+import {
+    AppBar,
+    Box,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon, ListItemText,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import CategoryIcon from '@mui/icons-material/Category';
+interface ListItemLinkProps {
+    icon?: React.ReactElement;
+    primary: string;
+    to: string;
+}
 export default function App() {
+    const [menuOpen, setMenuOpen] = React.useState(false);
+    const Link = React.forwardRef<HTMLAnchorElement, RouterLinkProps>(function Link(
+        itemProps,
+        ref,
+    ) {
+        return <RouterLink ref={ref} {...itemProps} role={undefined} />;
+    });
+    
+    function ListItemLink(props: ListItemLinkProps) {
+        const { icon, primary, to } = props;
+        
+        return (
+            <li>
+                <ListItem component={Link} to={to}>
+                    {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+                    <ListItemText primary={primary} />
+                </ListItem>
+            </li>
+        );
+    }
     return (
         <Router>
             <div className="App">
                 <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/species">Species</Link>
-                        </li>
-                        <li>
-                            <Link to="/people">People</Link>
-                        </li>
-                    </ul>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <AppBar position="static">
+                            <Toolbar>
+                                <IconButton
+                                    size="small"
+                                    edge="start"
+                                    color="inherit"
+                                    aria-label="menu"
+                                    sx={{ mr: 2 }}
+                                    onClick={() => setMenuOpen(!menuOpen)}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                    Star Wars Data Page
+                                </Typography>
+                                <Drawer
+                                    anchor={"left"}
+                                    open={menuOpen}
+                                    onClose={() => setMenuOpen(!menuOpen)}
+                                >
+                                    <List>
+                                        <ListItemLink to="/" primary="Home" icon={<HomeIcon />} />
+                                        <ListItemLink to="/species" primary="Species" icon={<CategoryIcon />} />
+                                        <ListItemLink to="/people" primary="People" icon={<PeopleIcon />} />
+                                    </List>
+                                </Drawer>
+                            </Toolbar>
+                        </AppBar>
+                    </Box>
                 </nav>
                 
                 {/* Use 'Routes' to wrap multiple 'Route' components */}
