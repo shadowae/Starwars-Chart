2401da9eb1dcedc5444ae10579fcf205c8b49892
Satish K <satish.appanasamy@gmail.com>
2023-08-01 20:26:12 +0800

    refactor: swapped out div for components in MUI

--- src/Species.tsx
+++ src/Species.tsx
@@ -1,5 +1,5 @@
 // Create a functional class called Species
-
+import {Container, Paper, Typography} from "@mui/material";
 import {useEffect} from "react";
 import {fetchAllSpeciesData} from "./API/getSpecies";
 import {SpeciesBarChart} from "./SpeciesBarChart";
@@ -17,17 +17,19 @@ const Species = () => {
     }, []);
 
     return (
-        <div className={"species-page"}>
-            <h1>Star Wars - Species</h1>
-            <div>
+        <Container sx={{backgroundColor: '#E5E5E5'}} maxWidth="xl">
+            <Typography variant={"h4"}>
+                Star Wars - Species
+            </Typography>
                 <GenericPieChart label={'Species Language Breakdown'} dataSet={speciesData} path={['language']} selectionMethod={'dropdown'}/>
                 <GenericPieChart label={'Species Classification Breakdown'} dataSet={speciesData} path={['classification']} selectionMethod={'buttons'}/>
                 <GenericPieChart label={'Species Designation Breakdown'} dataSet={speciesData} path={['designation']} selectionMethod={'buttons'}/>
-            </div>
-            <div className={'species-page-chart'}>
-                <SpeciesBarChart dataSet={speciesData}/>
-            </div>
-        </div>
+            <Container>
+                <Paper sx={{width: '90%', border: '5px dashed aquamarine'}}>
+                    <SpeciesBarChart dataSet={speciesData}/>
+                </Paper>
+            </Container>
+        </Container>
     )
 }
 
